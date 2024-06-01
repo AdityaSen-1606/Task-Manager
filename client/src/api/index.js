b72f82359b0ad62ec51634c589e61887eab65578
Mugen <adityasen1606@gmail.com>
2024-06-01 09:23:42 +0530

    Live Server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:'http://localhost:3000',
+    baseURL:'https://task-manager-57v9.onrender.com',
 });
 
 export const registerUser = (userData) => api.post('/api/auth/register',userData);
